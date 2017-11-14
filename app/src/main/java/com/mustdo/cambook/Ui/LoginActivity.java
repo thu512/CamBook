a8f1a72d28a7a1192a2a772eea4255a4cabbaf3f
lcj <thu512@naver.com>
2017-11-14 17:52:12 +0900

    --

--- app/src/main/java/com/mustdo/cambook/Ui/LoginActivity.java
+++ app/src/main/java/com/mustdo/cambook/Ui/LoginActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.NonNull;
 import android.text.TextUtils;
 
@@ -412,4 +414,39 @@ public class LoginActivity extends Activity implements GoogleApiClient.OnConnect
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
+
+
+
+
+    //백키에 대응하는 메소드
+    @Override
+    public void onBackPressed() {
+        //아래코드를 막으면 현재 화면의 종료처리가 중단됨
+        //super.onBackPressed();
+        if(!isFirstEnd){
+            //최초한번 백키를 눌렀다.
+            isFirstEnd=true;
+            //3초후에 초기화된다.(최초로 한번 백키를 눌렀던 상황이)
+            handler.sendEmptyMessageDelayed(1,3000);
+            U.getInstance().toast(this, "뒤로가기를 한번 더 누르시면 종료됩니다.");
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+    boolean isFirstEnd; //백키를 한번 눌렀나?
+
+    //핸들러, 메세지를 던져서 큐에 담고 하나씩 꺼내서 처리하는 메시징 시스템
+    Handler handler = new Handler(){
+        //이 메소드는 큐에 메세지가 존재하면 뽑아서 호출된다.
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if(msg.what == 0){ //최초로 백키를 한번 눌렀다.
+
+            }else if(msg.what == 1){ //3초가 지났다. 다시 초기화.
+                isFirstEnd=false;
+            }
+        }
+    };
 }
