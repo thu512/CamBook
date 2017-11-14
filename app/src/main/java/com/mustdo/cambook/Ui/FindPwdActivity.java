0ea1afb146ea4e581b8e14fea52eba534c6173c0
lcj <thu512@naver.com>
2017-11-14 10:44:18 +0900

    비밀번호 찾기

--- app/src/main/java/com/mustdo/cambook/Ui/FindPwdActivity.java
+++ app/src/main/java/com/mustdo/cambook/Ui/FindPwdActivity.java
@@ -3,11 +3,14 @@ package com.mustdo.cambook.Ui;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.mustdo.cambook.R;
 import com.mustdo.cambook.SuperActivity.Activity;
+import com.mustdo.cambook.Util.U;
 import com.mustdo.cambook.databinding.ActivityFindPwdBinding;
 
 public class FindPwdActivity extends Activity {
@@ -19,12 +22,40 @@ public class FindPwdActivity extends Activity {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_find_pwd);
 
+
+        binding.submit.setOnClickListener(view -> {
+            if (!isVaild()) {
+                return;
+            }
+
+            //비밀번호 재설정 메일 전송
+            FirebaseAuth auth = FirebaseAuth.getInstance();
+            String emailAddress = binding.id.getText().toString();
+
+            auth.sendPasswordResetEmail(emailAddress)
+                    .addOnCompleteListener(task -> {
+                        U.getInstance().log("이메일 전송 성공");
+                        U.getInstance().showPopup2(FindPwdActivity.this, "비밀번호 재설정", "비밀번호 재설정 메일이 전송되었습니다.",
+                                "확인",
+                                sweetAlertDialog -> {
+                                    sweetAlertDialog.dismissWithAnimation();
+                                    finish();
+                                });
+                    })
+                    .addOnFailureListener(e -> U.getInstance().toast(getApplicationContext(), "" + e.getMessage()));
+        });
+
+
         //글자입력시  지우기 버튼 생성
         binding.id.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
             @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
             @Override
             public void afterTextChanged(Editable editable) {
                 String s = editable.toString();
@@ -47,6 +78,20 @@ public class FindPwdActivity extends Activity {
         if (view.equals(binding.emailDelete)) {
             binding.id.setText("");
         }
+    }
+
+
+    //공백 검증
+    public boolean isVaild() {
+        String email = binding.id.getText().toString();
+
+
+        if (TextUtils.isEmpty(email)) {
+            binding.id.setError("이메일을 입력하세요.");
+            return false;
+        }
+
+        return true;
     }
 
 }
